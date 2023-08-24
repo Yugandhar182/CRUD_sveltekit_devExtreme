cb03c14ed7ea24a0e8f01959940a4006520fa54b
Yugandhar182 <yugandhar@recruitly.io>
2023-08-24 10:00:26 +0530

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,71 @@
 <script>
-    import { onMount } from "svelte";
-   
-   export let data;
-    const candidateData = data.candidatedata.data;
-  
-    // Prepare the grid data for dxDataGrid
-    const gridData = candidateData.map(item => ({
-      reference: item.id,
-      name: item.firstName,
-      email: item.email,
-      phone: item.mobile,
-      // Define other properties as needed
-    }));
-  
-    onMount(() => {
-      var dataGrid = new DevExpress.ui.dxDataGrid("#dataGrid", {
-        dataSource: gridData,
-        columns: [
-          { dataField: 'reference', caption: 'ID' },
-          { dataField: 'name', caption: 'Name' },
-          { dataField: 'email', caption: 'Email' },
-          { dataField: 'phone', caption: 'Mobile' },
-          // Define other columns as needed
-        ],
-      
+  import { onMount } from "svelte";
+ 
+ export let data;
+  const candidateData = data.candidatedata.data;
 
-        showBorders: true,
-        filterRow: {
-        visible: true,
-       },
-        
-       
-      paging: {
-      pageSize: 10,
+  // Prepare the grid data for dxDataGrid
+  const gridData = candidateData.map(item => ({
+  
+    firstName: item.firstName,
+    surname:item.surname,
+    email: item.email,
+    phone: item.mobile,
+    // Define other properties as needed
+  }));
+
+  onMount(() => {
+    var dataGrid = new DevExpress.ui.dxDataGrid("#dataGrid", {
+      dataSource: gridData,
+      columns: [
+        { dataField: 'firstName', caption: 'firstName' },
+        { dataField: 'surname', caption: 'surname' },
+        { dataField: 'email', caption: 'Email' },
+        { dataField: 'phone', caption: 'Mobile' },
+        // Define other columns as needed
+      ],
+    
+
+      showBorders: true,
+      filterRow: {
+      visible: true,
+     },
+     editing: {
+      allowDeleting: true,
+      allowAdding: true,
+      allowUpdating: true,
+      mode: "popup",
+      form: {
+        labelLocation: "top",
       },
+      popup: {
+        showTitle: true,
+      
+      },
+      texts: {
+        saveRowChanges: "Save",
+        cancelRowChanges: "Cancel",
+        deleteRow: "Delete",
+        confirmDeleteMessage: "Are you sure you want to delete this record?",
+      },
+    },
+      
+     
+    paging: {
+    pageSize: 10,
+    },
 
 // pagination:true,
 
-      pager: {
-       showPageSizeSelector: true,
-       allowedPageSizes: [ 10, 20],
-       showInfo: true,
-      },
-            });
+    pager: {
+     showPageSizeSelector: true,
+     allowedPageSizes: [ 10, 20],
+     showInfo: true,
+    },
           });
-        </script>
-        
-  <main>
-    <div id="dataGrid"></div>
-  </main>
-  
+        });
+      </script>
+      
+<main>
+  <div id="dataGrid"></div>
+</main>
